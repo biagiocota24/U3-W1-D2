5665e15051f8e97a45edcf516766be687f04c0e8
Biagio Cota <biagiocota24@gmail.com>
2026-04-29 16:19:23 +0200

    update

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -16,7 +16,7 @@
 // export default SingleBook;
 
 import { Component } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button,Alert } from "react-bootstrap";
 
 class SingleBook extends Component {
   state = {
@@ -25,16 +25,27 @@ class SingleBook extends Component {
   render() {
     const { book } = this.props;
     return (
-      <Card className="h-100 bg-dark text-light" onClick={()=>{
-        this.setState({selected : !this.state.selected})
-      }} style={{border: this.state.selected ? "2px solid red" : "none"}}>
-        <Card.Img variant="top" src={book.img} />
-        <Card.Body className="d-flex flex-column">
-          <Card.Title className="">{book.title}</Card.Title>
-          <Card.Text className="mb-auto">{book.category} book</Card.Text>
-          <Button variant="success">{book.price}€</Button>
-        </Card.Body>
-      </Card>
+      <div>
+        <Card
+          className="mb-3  bg-dark text-light"
+          onClick={() => {
+            this.setState({ selected: !this.state.selected });
+          }}
+          style={{ border: this.state.selected ? "2px solid red" : "none" }}
+        >
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body className="d-flex flex-column">
+            <Card.Title className="">{book.title}</Card.Title>
+            <Card.Text className="mb-auto">{book.category} book</Card.Text>
+            <Button variant="success">{book.price}€</Button>
+          </Card.Body>
+        </Card>
+        {this.state.selected === true && (
+          <Alert variant="danger" className="text-dark">
+            SELEZIONATO
+          </Alert>
+        )}
+      </div>
     );
   }
 }
